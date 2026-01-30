0a916340f5b65fdbf61e2cf7e08f03153f0b1df1
Matej Maňkoš <mmankos@protonmail.com>
2026-01-30 10:34:24 +0100

    enhancement(graphQLScraper): add proxy support to ensure GraphQL API request are not being blocked due to IP address blacklisting (#9)

--- src/index.mjs
+++ src/index.mjs
@@ -2,16 +2,16 @@ import fs from "node:fs";
 import pLimit from "p-limit";
 import { graphQLScrapeEvents } from "./scrapers/graphQLScraper.mjs";
 import {
-	htmlScrapeEventByID,
-	htmlScrapeEvents,
+    htmlScrapeEventByID,
+    htmlScrapeEvents,
 } from "./scrapers/htmlScraper.mjs";
 import { SourceTypes } from "./utils/constants.mjs";
 import {
-	constructUrl,
-	disableCursor,
-	enableCursor,
-	logError,
-	Spinner,
+    constructUrl,
+    disableCursor,
+    enableCursor,
+    logError,
+    Spinner,
 } from "./utils/utils.mjs";
 
 /**
@@ -32,109 +32,112 @@ import {
  * @param {number} [options.httpReqTimeout=5000] - Timeout (in milliseconds) for each HTTP request before it is aborted.
  * @param {boolean} [options.isAWS=true] - If true, only one puppeteer-core launch can be invoked concurrently, due to AWS Lambda limitations.
  * @param {string} [options.outputFile] - Optional file path to save the scraped events as JSON.
+ * @param {boolean} [options.useProxy=true] - If true, routes traffic through the local Tailscale SOCKS5 proxy (localhost:1055).
  *
  * @returns {Promise<Array>} Resolves to an array of scraped event objects.
  */
 export const scrapeEvents = async (
-	sourceTypes = { eventID: [], group: [], page: [], search_query: [] },
-	options = {
-		concurrency: 10,
-		derestrict: false,
-		eventsPerSourceLimit: undefined,
-		httpReqRetries: 5,
-		httpReqRetryDelay: 1000,
-		httpReqTimeout: 5000,
-		isAWS: true,
-		outputFile: undefined,
-	},
+    sourceTypes = { eventID: [], group: [], page: [], search_query: [] },
+    options = {
+        concurrency: 10,
+        derestrict: false,
+        eventsPerSourceLimit: undefined,
+        httpReqRetries: 5,
+        httpReqRetryDelay: 1000,
+        httpReqTimeout: 5000,
+        isAWS: true,
+        outputFile: undefined,
+        useProxy: true,
+    },
 ) => {
-	try {
-		disableCursor();
+    try {
+        disableCursor();
 
-		Object.assign(options, {
-			concurrency: options.concurrency ?? 10,
-			derestrict: options.derestrict ?? false,
-			eventsPerSourceLimit: options.eventsPerSourceLimit ?? undefined,
-			httpReqRetries: options.httpReqRetries ?? 5,
-			httpReqRetryDelay: options.httpReqRetryDelay ?? 1000,
-			httpReqTimeout: options.httpReqTimeout ?? 5000,
-			isAWS: options.isAWS ?? true,
-			outputFile: options.outputFile ?? undefined,
-		});
+        Object.assign(options, {
+            concurrency: options.concurrency ?? 10,
+            derestrict: options.derestrict ?? false,
+            eventsPerSourceLimit: options.eventsPerSourceLimit ?? undefined,
+            httpReqRetries: options.httpReqRetries ?? 5,
+            httpReqRetryDelay: options.httpReqRetryDelay ?? 1000,
+            httpReqTimeout: options.httpReqTimeout ?? 5000,
+            isAWS: options.isAWS ?? true,
+            outputFile: options.outputFile ?? undefined,
+            useProxy: options.useProxy ?? true,
+        });
 
-		const events = [];
-		const eventIDs = new Set();
-		const standaloneEventIDs = new Set();
-		const sourceLimit = pLimit(options.concurrency);
-		// only one puppeteer-core browser launch at a time on AWS Lambda is allowed
-		const graphQLLimit = pLimit(options.isAWS ? 1 : options.concurrency);
-		const spinner = new Spinner();
+        const events = [];
+        const eventIDs = new Set();
+        const standaloneEventIDs = new Set();
+        const sourceLimit = pLimit(options.concurrency);
+        // only one puppeteer-core browser launch at a time on AWS Lambda is allowed
+        const graphQLLimit = pLimit(options.isAWS ? 1 : options.concurrency);
+        const spinner = new Spinner();
 
-		const tasks = Object.entries(sourceTypes).flatMap(
-			([sourceType, sources]) => {
-				if (sourceType === SourceTypes.EventID) {
-					sources.forEach((id) => {
-						standaloneEventIDs.add(id);
-					});
-					return htmlScrapeEventByID(
-						{ standaloneEventIDs },
-						events,
-						eventIDs,
-						options,
-						spinner,
-					);
-				}
-				return sources.map((source) =>
-					sourceLimit(async () => {
-						const url = constructUrl(sourceType, source);
-						const hasNextPage = await htmlScrapeEvents(
-							url,
-							sourceType,
-							events,
-							eventIDs,
-							options,
-							spinner,
-						);
+        const tasks = Object.entries(sourceTypes).flatMap(
+            ([sourceType, sources]) => {
+                if (sourceType === SourceTypes.EventID) {
+                    sources.forEach((id) => {
+                        standaloneEventIDs.add(id);
+                    });
+                    return htmlScrapeEventByID(
+                        { standaloneEventIDs },
+                        events,
+                        eventIDs,
+                        options,
+                        spinner,
+                    );
+                }
+                return sources.map((source) =>
+                    sourceLimit(async () => {
+                        const url = constructUrl(sourceType, source);
+                        const hasNextPage = await htmlScrapeEvents(
+                            url,
+                            sourceType,
+                            events,
+                            eventIDs,
+                            options,
+                            spinner,
+                        );
 
-						if (sourceType !== SourceTypes.EventID && hasNextPage) {
-							await graphQLLimit(() =>
-								graphQLScrapeEvents(
-									url,
-									sourceType,
-									events,
-									eventIDs,
-									options,
-									spinner,
-								),
-							);
-						}
-					}),
-				);
-			},
-		);
+                        if (sourceType !== SourceTypes.EventID && hasNextPage) {
+                            await graphQLLimit(() =>
+                                graphQLScrapeEvents(
+                                    url,
+                                    sourceType,
+                                    events,
+                                    eventIDs,
+                                    options,
+                                    spinner,
+                                ),
+                            );
+                        }
+                    }),
+                );
+            },
+        );
 
-		await Promise.all(tasks);
+        await Promise.all(tasks);
 
-		if (options.outputFile) {
-			try {
-				await fs.promises.writeFile(
-					options.outputFile,
-					JSON.stringify(events, null, 2),
-					"utf8",
-				);
-			} catch (err) {
-				logError(err);
-			}
-		}
+        if (options.outputFile) {
+            try {
+                await fs.promises.writeFile(
+                    options.outputFile,
+                    JSON.stringify(events, null, 2),
+                    "utf8",
+                );
+            } catch (err) {
+                logError(err);
+            }
+        }
 
-		spinner.finish();
-		enableCursor();
+        spinner.finish();
+        enableCursor();
 
-		return events;
-	} catch (err) {
-		enableCursor();
-		logError(err);
+        return events;
+    } catch (err) {
+        enableCursor();
+        logError(err);
 
-		throw err;
-	}
+        throw err;
+    }
 };
